c0f4afae53dcfe9826b3df7f168a1e9f2f1ad8a4
JunCLi <kevinjcli1994@gmail.com>
2020-10-27 19:35:37 -0400

    modified resolvers.js for less maintenance intensive way to add additional resolvers

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -1,13 +1,29 @@
 const userResolvers = require('./usersResolvers')
 
+const resolvers = [
+	userResolvers,
+]
+
+const queries = {}
+const mutations = {}
+
+resolvers.forEach(resolver => {
+	for (let query in resolver.Query) {
+		queries[query] = resolver.Query[query]
+	}
+	for (let mutation in resolver.Mutation) {
+		mutations[mutation] = resolver.Mutation[mutation]
+	}
+})
+
 module.exports = () => {
   return {
     Query: {
-			...userResolvers.Query
+			...queries,
 		},
 
 		Mutation: {
-			...userResolvers.Mutation
+			...mutations,
 		},
   }
 }
